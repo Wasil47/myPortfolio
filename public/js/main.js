3d7ebdfaf37d71770a1ebe94e4803548e0e0fbb3
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-02-17 15:19:18 +0100

    add few sections

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 const navBar = document.getElementById("navbar");
 const hamMenu = document.querySelector(".ham");
 
+document.addEventListener("DOMContentLoaded", ()=>{ 
+  navBar.classList.toggle("hide");
+  hamMenu.classList.toggle("active");
+});
+
+
 hamMenu.addEventListener("click", () => {
   navBar.classList.toggle("hide");
   hamMenu.classList.toggle("active");
