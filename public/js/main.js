c22dfe3229d101cb4fa5d15151c2bac012bfddd0
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-02-27 13:18:15 +0100

    add animations. Portfolio v1.0

--- public/js/main.js
+++ public/js/main.js
@@ -5,19 +5,99 @@ slides = document.getElementById('slides'),
 slideArr = document.getElementsByClassName('slide'),
 // slideArr = document.querySelectorAll(".slide"),
 prev = document.getElementById('prev'),
-next = document.getElementById('next');
+next = document.getElementById('next'),
+html = document.querySelector('html');
 
+// START NAVBAR Hide/Show 
 document.addEventListener("DOMContentLoaded", ()=>{ 
   navBar.classList.toggle("hide");
   hamMenu.classList.toggle("active");
 });
-
 hamMenu.addEventListener("click", () => {
   navBar.classList.toggle("hide");
   hamMenu.classList.toggle("active");
 });
+// END NAVBAR Hide/Show 
 
+//START Scroll to section
+const scrollLink = document.querySelectorAll('a[href^="#"]');
+let fSize = window.getComputedStyle(html, null).getPropertyValue('font-size');
 
+scrollLink.forEach(link => {
+  link.addEventListener('click', el => {
+    el.preventDefault();
+    const fValue = parseInt(fSize);
+    const targetEl = document.querySelector(link.getAttribute('href'));
+    const posY = targetEl.offsetTop - (fValue*3);    
+    window.scroll(0, posY);
+  });
+});
+//END Scroll to section
+
+//START Animate (appear) Items
+function throttle(f, wait) {
+  let time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      f();
+      time = Date.now();
+    }
+  }
+};
+const animateItems = ()=> {
+  const windowHeight = window.innerHeight;
+  const aboutSection = document.getElementById('about-section');
+  const skillSection = document.getElementById('skill-section');
+  const contactSection = document.getElementById('contact-section');
+
+  const aboutDistTop = aboutSection.getBoundingClientRect().top;
+  const skillDistTop = skillSection.getBoundingClientRect().top;
+  const skillDistBot = skillSection.getBoundingClientRect().bottom;
+  const contactlDistTop = contactSection.getBoundingClientRect().top;
+
+  const aboutVisible = aboutDistTop + windowHeight * 0 < windowHeight;
+  const skillVisible = skillDistTop + windowHeight * 0.3 < windowHeight;
+  const skillNotVisible = skillDistBot + windowHeight * 0.5 < windowHeight;
+  const contactVisible = contactlDistTop + windowHeight * 0 < windowHeight;    
+    
+  if (aboutVisible) {
+    document.querySelector('.about').classList.remove('hide');
+  } 
+  if (skillVisible && !skillNotVisible) {
+    document.querySelectorAll('.skills').forEach((skill, i)=>{
+      setTimeout(()=>{skill.classList.remove('slide-up')}, 200*i);
+    });
+  }
+  if (skillNotVisible || !skillVisible && !skillNotVisible) {
+    document.querySelectorAll('.skills').forEach((skill, i)=>{
+      setTimeout(()=>{skill.classList.add('slide-up')}, 200*i);      
+    });
+  }
+  if (contactVisible) {    
+    const bounce = ()=> {
+      document.querySelectorAll('.contact-text').forEach((contact, i)=>{      
+        setTimeout(()=>{contact.classList.add('bounce')}, 1000*i);
+        contact.onanimationend = () => {
+         contact.classList.remove('bounce');
+        };
+      });
+    }
+    setTimeout(bounce, 500);
+  }
+};
+window.addEventListener('scroll', throttle(animateItems, 200));
+window.addEventListener('DOMContentLoaded', ()=>{
+  const helloClass = document.getElementsByClassName('hello')[0].children;
+  const hello = () => {
+    for (let i = 0; i < helloClass.length; i++) {
+    setTimeout(()=>{helloClass[i].classList.remove('hide-hello')}, 500*i);
+    }
+  };
+  setTimeout(hello, 200);  
+});
+//STOP Animate (appear) Items
+
+// START: InfinitySlider (carousel)
 const slide = ()=>{
   let 
   // posInitial = slides.offsetLeft,
@@ -64,8 +144,8 @@ const slide = ()=>{
       posInitial = -slideSize;
     }
     shiftReady = true;
-  };
-  
+  };  
+
   // checkPos when transition end
   slides.addEventListener('transitionend', checkPosition); 
     
@@ -77,5 +157,5 @@ const slide = ()=>{
     posInitial = -slideSize;
   });
 };
-
 slide();
+// END: InfinitySlider (carousel)
